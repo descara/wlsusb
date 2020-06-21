e126ef54728af9263d94576c1512335337096b85
descara <reubenej@gmail.com>
2020-06-21 17:42:50 +1000

    Add project files.

---
+++ wlsusb/main.cpp
@@ -0,0 +1,158 @@
+#define _WIN32_DCOM
+#include <iostream>
+#include <comdef.h>
+#include <WbemIdl.h>
+
+
+int main(int argc, char **argv)
+{	
+	HRESULT hres;
+
+	// Initalize COM
+	hres = CoInitialize(0);
+	
+	// Handle Error
+	if (FAILED(hres))
+	{
+		std::cout << "Failed to initialize COM library. Error code = 0x"
+			<< std::hex << hres << std::endl;
+		return 1;
+	}
+
+	// Set up general COM security levels
+	hres = CoInitializeSecurity(
+		NULL,
+		-1,                          // COM authentication
+		NULL,                        // Authentication services
+		NULL,                        // Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+		NULL,                        // Authentication info
+		EOAC_NONE,                   // Additional capabilities 
+		NULL                         // Reserved
+	);
+
+
+	if (FAILED(hres))
+	{
+		std::cout << "Failed to initialize security. Error code = 0x"
+			<< std::hex << hres << std::endl;
+		CoUninitialize();
+		return 1;                    // Program has failed.
+	}
+
+	// Obtain the initial locator to WMI 
+	IWbemLocator *pLoc = NULL;
+	hres = CoCreateInstance(
+		CLSID_WbemLocator,
+		0,
+		CLSCTX_INPROC_SERVER,
+		IID_IWbemLocator, (LPVOID*) &pLoc);
+
+	if (FAILED(hres))
+	{
+		std::cout << "Failed to create IWbemLocator object."
+			<< " Err code = 0x"
+			<< std::hex << hres << std::endl;
+		CoUninitialize();
+		return 1;                 // Program has failed.
+	}
+
+	// Connect to WMI through the IWbemLocator::ConnectServer method
+	IWbemServices *pSvc = NULL;
+	hres = pLoc->ConnectServer(
+		_bstr_t(L"ROOT\\CIMV2"),
+		NULL,
+		NULL,
+		0,
+		NULL,
+		0,
+		0,
+		&pSvc // Pointer to IWbemServices proxy
+	);
+
+	if (FAILED(hres))
+	{
+		std::cout << "Could not connect. Error code = 0x"
+			<< std::hex << hres << std::endl;
+		pLoc->Release();
+		CoUninitialize();
+		return 1;                // Program has failed.
+	}
+
+	std::cout << "Connected to ROOT\\CIMv2 WMI namespace" << std::endl;
+	
+	// Set security levels on the proxy
+
+	hres = CoSetProxyBlanket(
+		pSvc,                        // Indicates the proxy to set
+		RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+		RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+		NULL,                        // Server principal name 
+		RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+		NULL,                        // client identity
+		EOAC_NONE                    // proxy capabilities 
+	);
+
+	if (FAILED(hres))
+	{
+		std::cout << "Could not set proxy blanket. Error code = 0x"
+			<< std::hex << hres << std::endl;
+		pSvc->Release();
+		pLoc->Release();
+		CoUninitialize();
+		return 1;               // Program has failed.
+	}
+
+	// Use the IWbemServices pointer to make requests of WMI
+  // For example, get the name of the operating system
+	IEnumWbemClassObject* pEnumerator = NULL;
+	hres = pSvc->ExecQuery(
+		bstr_t("WQL"),
+		bstr_t("SELECT * FROM Win32_OperatingSystem"),
+		WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+		NULL,
+		&pEnumerator);
+
+	if (FAILED(hres))
+	{
+		std::cout << "Query for operating system name failed."
+			<< " Error code = 0x"
+			<< std::hex << hres << std::endl;
+		pSvc->Release();
+		pLoc->Release();
+		CoUninitialize();
+		return 1;               // Program has failed.
+	}
+
+	IWbemClassObject* pclsObj = NULL;
+	ULONG uReturn = 0;
+
+	while (pEnumerator)
+	{
+		HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1,
+			&pclsObj, &uReturn);
+
+		if (0 == uReturn)
+		{
+			break;
+		}
+
+		VARIANT vtProp;
+
+		// Get the value of the Name property
+		hr = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
+		std::wcout << " OS Name : " << vtProp.bstrVal << std::endl;
+		VariantClear(&vtProp);
+
+		pclsObj->Release();
+	}
+
+	pSvc->Release();
+	pLoc->Release();
+	pEnumerator->Release();
+	CoUninitialize();
+
+	return 0;
+}
